cb21698df279add0d48fc43702df156cd5ef0a8e
flklr-dev <kitadriand@gmail.com>
2024-12-19 22:51:00 +0800

    Add ScrollToTop component to App for improved navigation experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import ScrollToTop from './components/ScrollToTop';
 import HomeScreen from './components/HomeScreen';
 import AboutMe from './components/AboutMe';
 import ContactScreen from './components/ContactScreen';
@@ -12,6 +13,7 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/about" element={<AboutMe />} />
