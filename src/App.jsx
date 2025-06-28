42d3d21339b635b4bab5ef8d59bf3767876665c6
flklr-dev <kitadriand@gmail.com>
2025-06-28 12:59:25 +0800

    Delete other files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ScrollToTop from './components/ScrollToTop';
 import HomeScreen from './components/HomeScreen';
-import AboutMe from './components/AboutMe';
-import ContactScreen from './components/ContactScreen';
-import PortfolioScreen from './components/PortfolioScreen';
-import BlogPost from './components/BlogPost';
-import ResumeScreen from './components/ResumeScreen';
 
 function App() {
   return (
@@ -16,11 +10,6 @@ function App() {
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomeScreen />} />
-        <Route path="/about" element={<AboutMe />} />
-        <Route path="/portfolio" element={<PortfolioScreen />} />
-        <Route path="/contact" element={<ContactScreen />} />
-        <Route path="/blog/:id" element={<BlogPost />} />
-        <Route path="/resume" element={<ResumeScreen />} />
       </Routes>
     </Router>
   );
